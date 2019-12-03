a7c16a1c4a670a02692777c39f530ba95981b8e9
dyik <51643039+dyik@users.noreply.github.com>
2019-12-03 09:34:15 +0700

    Update sms_to_telegram.php

--- sms_to_telegram.php
+++ sms_to_telegram.php
@@ -3,10 +3,10 @@
  * DON'T FORGET TO CONFIGURE!
  * See Readme.md
  */
-define('CHAT_ID', 0);
+define('CHAT_ID', @yeeckhack_bot);
 //bot @username
-define('BOT_NAME', 'username');
-define('API_KEY', '');
+define('BOT_NAME', '');
+define('API_KEY', '1034803622:AAG7erncR1qOR6ITgLc30FMcCcPFuJkbNk0');
 
 require __DIR__ . '/vendor/autoload.php';
 use Longman\TelegramBot\Request;
