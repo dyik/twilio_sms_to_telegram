05ca7ddce45eee4c994f03f417ba357bc54bdc1d
dyik <51643039+dyik@users.noreply.github.com>
2019-12-03 11:10:49 +0700

    Update sms_to_telegram.php

--- sms_to_telegram.php
+++ sms_to_telegram.php
@@ -3,8 +3,8 @@
  * DON'T FORGET TO CONFIGURE!
  * See Readme.md
  */
-define('CHAT_ID', '843860963');//bot @username 
-define('BOT_NAME', '@yeeckhack_bot');
+define('CHAT_ID', '843860963');
+//bot @username define('BOT_NAME', '@yeeckhack_bot');
 define('API_KEY', '1034803622:AAG7erncR1qOR6ITgLc30FMcCcPFuJkbNk0');
 
 require __DIR__ . '/vendor/autoload.php';
